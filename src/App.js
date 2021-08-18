f531e40ca26c9ebbe699c65e952857ea1dfd0d56
kongtaemin <rhdxoals@gmail.com>
2021-08-18 15:17:16 +0900

    유튜브API 인기영상목록 불러오기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import VideoList from "./components/video_list";
+import React, { useEffect, useState } from "react";
+import dotenv from "dotenv";
+dotenv.config();
 
 function App() {
-  return <div>HELLO KONG</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?key=${process.env.REACT_APP_APIKEY}&part=snippet&chart=mostPopular&maxResults=20`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
